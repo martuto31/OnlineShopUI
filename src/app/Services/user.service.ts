b798d0d0ae0b458f6666bd69d1dec40acce1ddf5
Martin Petrov <martuto31@gmail.com>
2023-05-01 04:24:44 +0300

    Added Models, Components and Services folders; Added user model; Added user service;

---
+++ src/app/Services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../Models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUserUrl = 'https://localhost:7260/api/User';
+
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(user: User): Observable<any> {
+    return this.http.post<any>(`${this.apiUserUrl}/Register`, user);
+  }
+}
